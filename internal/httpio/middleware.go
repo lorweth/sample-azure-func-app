4e7508540d8b9ccab2a97e2b0f64da64c1fce3fa
Đặng Hữu Lộc <44874068+virsavik@users.noreply.github.com>
2023-12-13 21:51:11 +0700

    Setup tracer middleware (#1)
    * Add OTEL configurations
    
    * Add trace span to root middleware
    
    * Add OTEL env to app settings
    
    * Add trace id to logger

--- internal/httpio/middleware.go
+++ internal/httpio/middleware.go
@@ -2,6 +2,11 @@ package httpio
 
 import (
 	"fmt"
+	"go.opentelemetry.io/otel"
+	"go.opentelemetry.io/otel/attribute"
+	"go.opentelemetry.io/otel/propagation"
+	semconv "go.opentelemetry.io/otel/semconv/v1.21.0"
+	"go.opentelemetry.io/otel/trace"
 	"net/http"
 	"runtime/debug"
 
@@ -10,12 +15,42 @@ import (
 	"github.com/virsavik/sample-azure-func-app/internal/logger"
 )
 
+const (
+	scopeName = "github.com/virsavik/sample-azure-func-app/internal/httpio"
+	version   = "1.0.0"
+)
+
 // Middleware is a customizable HTTP middleware that provides logging, panic recovery,
 // and response wrapping capabilities.
 func Middleware(log logger.Logger) func(next http.Handler) http.Handler {
+	tp := otel.GetTracerProvider()
+	tracer := tp.Tracer(scopeName,
+		trace.WithInstrumentationVersion(version),
+	)
+	propagators := otel.GetTextMapPropagator()
+
 	return func(next http.Handler) http.Handler {
 		// The main middleware function that will be executed for each HTTP request.
 		fn := func(w http.ResponseWriter, r *http.Request) {
+			ctx := propagators.Extract(r.Context(), propagation.HeaderCarrier(r.Header))
+
+			ctx, span := tracer.Start(
+				ctx,
+				"http.request",
+				trace.WithAttributes(
+					semconv.HostName(r.Host),
+					semconv.URLPath(r.URL.Path),
+					semconv.URLQuery(r.URL.RawQuery),
+					semconv.HTTPRequestMethodOriginal(r.Method),
+					semconv.HTTPRequestBodySize(int(r.ContentLength)),
+					attribute.String("http.request.proto", r.Proto),
+					attribute.String("http.request.remote_address", r.RemoteAddr),
+					semconv.UserAgentOriginal(r.UserAgent()),
+				),
+				trace.WithSpanKind(trace.SpanKindServer),
+			)
+			defer span.End()
+
 			// Wrap response for getting response status and body size.
 			ww := middleware.NewWrapResponseWriter(w, r.ProtoMajor)
 
@@ -51,6 +86,7 @@ func Middleware(log logger.Logger) func(next http.Handler) http.Handler {
 					logger.String("user_agent.original", r.UserAgent()),
 					logger.Int("http.response.status_code", ww.Status()),
 					logger.Int("http.response.body.size", ww.BytesWritten()),
+					logger.String("trace.id", trace.SpanFromContext(ctx).SpanContext().TraceID().String()),
 				)
 
 				reqLogger.Infof("Served")
@@ -58,7 +94,13 @@ func Middleware(log logger.Logger) func(next http.Handler) http.Handler {
 
 			// Handle the original request by calling the next handler in the chain,
 			// passing the wrapped response writer and the modified request context.
-			next.ServeHTTP(ww, r.WithContext(logger.SetInCtx(r.Context(), log)))
+			next.ServeHTTP(ww, r.WithContext(logger.SetInCtx(ctx, log)))
+
+			// Add the status code as an attribute to the span.
+			span.SetAttributes(
+				semconv.HTTPResponseStatusCode(ww.Status()),
+				semconv.HTTPResponseBodySize(ww.BytesWritten()),
+			)
 		}
 
 		// Return an HTTP handler function wrapping the main middleware function.
